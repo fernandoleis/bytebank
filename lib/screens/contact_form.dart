f20031e9dc4b1a8733601a67a6be16b5e0ae9998
Fernando Leis <fleis@rd.com.br>
2021-07-07 21:28:54 -0300

    criando model

--- lib/screens/contact_form.dart
+++ lib/screens/contact_form.dart
@@ -1,7 +1,17 @@
+import 'package:estudo/models/contact.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ContactForm extends StatelessWidget {
+class ContactForm extends StatefulWidget {
   const ContactForm({Key? key}) : super(key: key);
+
+  @override
+  _ContactFormState createState() => _ContactFormState();
+}
+
+class _ContactFormState extends State<ContactForm> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _accountNumberController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -14,12 +24,14 @@ class ContactForm extends StatelessWidget {
         child: Column(
           children: [
             TextField(
+              controller: _nameController,
               decoration: InputDecoration(labelText: 'Full Name'),
               style: TextStyle(fontSize: 24.0),
             ),
             Padding(
               padding: const EdgeInsets.only(top: 8.0),
               child: TextField(
+                controller: _accountNumberController,
                 decoration: InputDecoration(labelText: 'Account Number'),
                 style: TextStyle(fontSize: 24.0),
                 keyboardType: TextInputType.number,
@@ -31,7 +43,12 @@ class ContactForm extends StatelessWidget {
                 width: double.maxFinite,
                 child: ElevatedButton(
                   child: Text('Confirmar'),
-                  onPressed: () {},
+                  onPressed: () {
+                    final String name = _nameController.text;
+                    final int? accountNumber = int.tryParse(_accountNumberController.text);
+                    final Contact newContact = Contact(name, accountNumber);
+                    Navigator.pop(context,newContact);
+                  },
                 ),
               ),
             )
--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -1,3 +1,4 @@
+import 'package:estudo/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
 class ContactsList extends StatelessWidget {
@@ -10,15 +11,33 @@ class ContactsList extends StatelessWidget {
         title: Text('Contacts'),
       ),
       body: ListView(
-        children: [Card(
-          child: ListTile(
-            title: Text('Fernando', style: TextStyle(fontSize: 24.0),) ,
-            subtitle: Text('1000', style: TextStyle(fontSize: 16.0),),
-          ),
-        )],
+        children: [
+          Card(
+            child: ListTile(
+              title: Text(
+                'Fernando',
+                style: TextStyle(fontSize: 24.0),
+              ),
+              subtitle: Text(
+                '1000',
+                style: TextStyle(fontSize: 16.0),
+              ),
+            ),
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: () {
+          Navigator.of(context)
+              .push(
+                MaterialPageRoute(
+                  builder: (context) => ContactForm(),
+                ),
+              )
+              .then(
+                (value) => debugPrint(value.toString()),
+              );
+        },
         child: Icon(Icons.add),
       ),
     );
